c86d24812504750ea5bf767e7c5015779562afcc
PlatenPrime <proshta@gmail.com>
2023-01-07 11:43:54 +0200

    07.01.2023

---
+++ pages/RowsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const RowsPage = () => {
+
+
+	return (
+		<Text>
+			Здесь будет грузиться список рядов
+		</Text>
+	);
+};
+
+export default RowsPage;
