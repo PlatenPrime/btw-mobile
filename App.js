6c49c4ed3c6c6d11631e03027f3c2a5526c3f207
PlatenPrime <proshta@gmail.com>
2023-01-14 12:29:56 +0200

    14.01.2023

--- App.js
+++ App.js
@@ -6,17 +6,6 @@ import { Button, FlatList, Image, ScrollView, StyleSheet, Text, TextInput, View 
 
 
 
-import RowsPage from './pages/RowsPage';
-
-
-
-const items = [
-	{ id: '0', text: 'View' },
-	{ id: '1', text: 'Text' },
-	{ id: '2', text: 'Image' },
-	{ id: '3', text: 'ScrollView' },
-	{ id: '4', text: 'ListView' },
-]
 
 
 
@@ -31,17 +20,7 @@ export default function App() {
 
 		<View style={styles.container}>
 
-
-			<FlatList
-
-				data={items}
-				renderItem={({ item }) => <Text style={styles.row}>{item.text}</Text>}
-				keyExtractor={(item) => item.id}
-			/>
-
-			<RowsPage />
-
-
+			<Text style={styles.text} >Hello world!</Text>
 
 		</View>
 
@@ -52,45 +31,13 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-
+		backgroundColor: '#25292e',
+		alignItems: 'center',
+		justifyContent: 'center',
 	},
 	text: {
-		color: 'rgb(59,108,212)',
-		fontSize: 42,
-		fontWeight: '100',
-		textAlign: 'center',
-	},
-	box: {
-		width: 150,
-		height: 150,
-		backgroundColor: '#3B6CD4',
-		borderWidth: 1,
-		borderColor: 'black',
-		borderRadius: 4,
-	},
-	image: {
-		width: 200,
-		height: 200,
-	},
-	small: {
-		width: 200,
-		height: 200,
-		marginBottom: 10,
-		marginRight: 10,
-		backgroundColor: 'skyblue',
-	},
-	large: {
-		width: 300,
-		height: 300,
-		marginBottom: 10,
-		marginRight: 10,
-		backgroundColor: 'steelblue',
-	},
-	row: {
-		padding: 15,
-		marginBottom: 5,
-		backgroundColor: 'skyblue',
-	},
+		color: "whitesmoke",
+		fontSize: 32,
+	}
+
 })
