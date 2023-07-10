2b1612283490c45ef987ea01de387c82659c7baa
PlatenPrime <proshta@gmail.com>
2023-07-10 07:42:02 +0300

    10.07.2023

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
+
+import useFetch from '../../../hook/useHook'
+import { useRouter } from "expo-router";
 
 import styles from './nearbyjobs.style'
 
+import { COLORS } from "../../../constants";
+
+import NearbyJobsCard from "../../common/cards/nearby/NearbyJobCard"
+
+
 const Nearbyjobs = () => {
-  return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
-  )
+
+	const router = useRouter();
+
+	const { data, isLoading, error } = useFetch("search", {
+		query: "React developer",
+		num_pages: "1",
+	});
+
+
+
+
+
+
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.header} >
+				<Text style={styles.headerTitle} >Nearby Jobs</Text>
+				<TouchableOpacity
+					onPress={() => { }}
+				>
+					<Text style={styles.headerBtn}>Show All</Text>
+				</TouchableOpacity>
+			</View>
+
+
+			<View style={styles.cardsContainer} >
+				{isLoading ? (
+					<ActivityIndicator size="large" colors={COLORS.primary} />
+				) : error ? (
+					<Text>Something went wrong</Text>
+				) : (
+					data?.map((job) => (
+						<NearbyJobsCard
+							job={job}
+							key={`nearby-job-${job?.job_id}`}
+							handlerNavigate={() => router.push(`/job-details/${job.job_id}`)}
+
+
+						/>
+					))
+				)}
+			</View>
+
+
+		</View>
+	)
 }
 
 export default Nearbyjobs
