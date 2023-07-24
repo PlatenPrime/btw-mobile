6a03f88420ba1ec687e661c4399e63e5e6c56fe5
PlatenPrime <proshta@gmail.com>
2023-07-24 08:14:30 +0300

    24.07.2023

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,52 @@
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router/tabs';
+export default function AppLayout() {
+	return (
+		<Tabs>
+			<Tabs.Screen
+				// Name of the route to hide.
+				name="index"
+				options={{
+					// This tab will no longer show up in the tab bar.
+					href: null,
+				}}
+			/>
+			<Tabs.Screen
+				// Name of the route to hide.
+				name="zapasi"
+				options={{
+					headerTitle: "Sklad",
+					// This tab will no longer show up in the tab bar.
+					href: '/zapasi',
+				}}
+			/>
 
-export default function Layout() {
-	return <Stack />;
+			<Tabs.Screen
+				// Name of the route to hide.
+				name="zones"
+				options={{
+					headerTitle: "Zones",
+					// This tab will no longer show up in the tab bar.
+					href: '/zones',
+				}}
+			/>
+
+			<Tabs.Screen
+				// Name of the route to hide.
+				name="search"
+				options={{
+					headerTitle: "Search",
+					// This tab will no longer show up in the tab bar.
+					href: '/search',
+				}}
+			/>
+
+
+
+
+
+
+
+
+		</Tabs>
+	);
 }
---
+++ app/zapasi.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const zapasi = () => {
+	return (
+		<View>
+			<Text>zapasi</Text>
+		</View>
+	)
+}
+
+export default zapasi
