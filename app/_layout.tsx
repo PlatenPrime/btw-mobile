dea86cf2f04e02b0da028d71bd49f587081ea028
PlatenPrime <proshta@gmail.com>
2023-11-19 11:56:05 +0200

    19.11.2023

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { Stack, useRouter } from 'expo-router'
+import { Tabs } from 'expo-router'
+import { AntDesign } from '@expo/vector-icons';
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 export default function _layout() {
 
@@ -9,44 +12,141 @@ export default function _layout() {
 
 
 	return (
-		<Stack
-			screenOptions={{
-				headerStyle: { backgroundColor: "black" },
-				headerTintColor: "skyblue"
-			}}
-
-		>
-			<Stack.Screen
+		<Tabs>
+			<Tabs.Screen
 				name="index"
-				options={{
-					title: "Home"
-				}} />
-			<Stack.Screen
-				name="register/index"
-				options={{
-					title: "Register Page",
-					headerRight: () => (<Button
-						title="Login"
-						onPress={() => { router.push("/login") }} />
-					)
-				}} />
-			<Stack.Screen
-				name="login"
-				options={{
-					title: "Login Modal",
-					presentation: "modal"
-				}} />
-			<Stack.Screen
+				options={
+					{
+						headerTitle: "Головна",
+						tabBarLabel: "Головна",
+						tabBarIcon: () => <AntDesign name="home" size={24} color="black" />
+					}
+				}
+			/>
+
+			<Tabs.Screen
+				name="btw"
+				options={
+					{
+						headerTitle: "BTW",
+						tabBarLabel: "BTW",
+						tabBarIcon: () => <SimpleLineIcons name="menu" size={24} color="black" />
+					}
+				}
+			/>
+
+			<Tabs.Screen
+				name="settings"
+				options={
+					{
+						headerTitle: "Налаштування",
+						tabBarLabel: "Профіль",
+						tabBarIcon: () => <AntDesign name="setting" size={24} color="black" />
+					}
+				}
+			/>
+
+
+
+
+
+
+			<Tabs.Screen
 				name="(tabs)"
 				options={{
-					headerShown: false
-				}} />
+					// This tab will no longer show up in the tab bar.
+					href: null,
+				}
+				}
+			/>
+			<Tabs.Screen
+				name="login"
+				options={{
+					// This tab will no longer show up in the tab bar.
+					href: null,
+				}
+				}
+			/>
+			<Tabs.Screen
+				name="register/index"
+				options={{
+					// This tab will no longer show up in the tab bar.
+					href: null,
+				}
+				}
+			/>
 
-			<Stack.Screen
+
+			{/* <Tabs.Screen
+				name="posts"
+				options={
+					{
+						headerShown: false,
+						headerTitle: "Posts",
+						tabBarLabel: "Posts"
+					}
+				}
+			/> */}
+			<Tabs.Screen
 				name="[missing]"
 				options={{
-					title: "Not Found"
-				}} />
-		</Stack>
+					// This tab will no longer show up in the tab bar.
+					href: null,
+				}
+				}
+			/>
+
+
+
+
+		</Tabs>
+
+
+
+
+
+
+
+
+
+		// <Stack
+		// 	screenOptions={{
+		// 		headerStyle: { backgroundColor: "black" },
+		// 		headerTintColor: "skyblue"
+		// 	}}
+
+		// >
+		// 	<Stack.Screen
+		// 		name="index"
+		// 		options={{
+		// 			title: "Home"
+		// 		}} />
+		// 	<Stack.Screen
+		// 		name="register/index"
+		// 		options={{
+		// 			title: "Register Page",
+		// 			headerRight: () => (<Button
+		// 				title="Login"
+		// 				onPress={() => { router.push("/login") }} />
+		// 			)
+		// 		}} />
+		// 	<Stack.Screen
+		// 		name="login"
+		// 		options={{
+		// 			title: "Login Modal",
+		// 			presentation: "modal"
+		// 		}} />
+		// 	<Stack.Screen
+		// 		name="(tabs)"
+		// 		options={{
+		// 			headerShown: false
+		// 		}} />
+
+		// 	<Stack.Screen
+		// 		name="[missing]"
+		// 		options={{
+		// 			title: "Not Found"
+		// 		}} />
+		// </Stack>
 	)
 }
