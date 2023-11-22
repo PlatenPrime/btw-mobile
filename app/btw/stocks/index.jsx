80ad27e04baf0bcfd7ae9345798655de4c3a043b
PlatenPrime <proshta@gmail.com>
2023-11-22 12:28:54 +0200

    Update index.jsx

--- app/btw/stocks/index.jsx
+++ app/btw/stocks/index.jsx
@@ -1,25 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useRowStore } from '../../../stores/rowsStore';
 import ScreenContainer from '../../../components/ScreenContainer';
 import { Link } from 'expo-router';
-import { FlatList } from 'react-native-gesture-handler';
-import axios from 'axios';
+
 
 export default function Stocks() {
-	const rows = useRowStore((state) => state.rows);
-	const getAllRows = useRowStore((state) => state.getAllRows);
 
 
+	const [data, setData] = useState({})
 
 
+	const storeData = async (value) => {
+		try {
+			const jsonValue = JSON.stringify(value);
+			await AsyncStorage.setItem('rows', jsonValue);
+		} catch (e) {
+			// saving error
+		}
+	};
 
+
+	const getData = async () => {
+		try {
+			const jsonValue = await AsyncStorage.getItem('rows');
+			const res = jsonValue != null ? JSON.parse(jsonValue) : null;
+			setData(res)
+			return res
+		} catch (e) {
+			// error reading value
+		}
+	};
 
 
 
 	return (
 		<ScreenContainer>
+
+
+			<Button title="Записать" onPress={() => storeData({
+				row: "10-12",
+				pallets: "mnogo"
+			})} />
+			<Button title="Прочитать" onPress={getData} />
+
+
+
 			<Text
 				className="text-2xl text-white text-center"
 			>
@@ -43,6 +69,9 @@ export default function Stocks() {
 			>
 				Ряд 17-19
 			</Link>
+
+			<Text>{data?.row}</Text>
+			<Text>{data?.pallets}</Text>
 
 
 
