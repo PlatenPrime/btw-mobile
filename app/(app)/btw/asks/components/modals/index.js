34a9f03a2560a37b1213834b322fc1f7720589c6
Aleksandr Marchenko <proshta@gmail.com>
2025-01-15 13:01:26 +0200

    15.01.2025

--- app/(app)/btw/asks/components/modals/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ModalCreateAsk } from './ModalCreateAsk';
-export { default as ModalUpdateAskPos } from './ModalUpdateAskPos';
-export { default as ModalDeleteAsk } from './ModalDeleteAsk';
-export { default as ModalSolveAsk } from './ModalSolveAsk';
-export { default as ModalFailAsk } from './ModalFailAsk';
--- app/(app)/btw/pallets/components/modals/index.js
+++
@@ -1,7 +0,0 @@
-export { default as ModalClearPallet } from './ModalClearPallet';
-export { default as ModalCreatePos } from './ModalCreatePos';
-export { default as ModalDeletePallet } from './ModalDeletePallet';
-export { default as ModalDeletePos } from './ModalDeletePos';
-export { default as ModalMovePalletContent } from './ModalMovePalletContent';
-export { default as ModalUpdatePallet } from './ModalUpdatePallet';
-export { default as ModalUpdatePos } from './ModalUpdatePos';
